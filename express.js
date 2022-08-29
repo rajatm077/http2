a19a79dace317f07e1db796869a9ed47a7370003
rajat_malhotra <rajatmalhotra26@gmail.com>
2022-08-29 22:59:07 +0530

    http1 server setup

---
+++ express.js
@@ -0,0 +1,15 @@
+const express = require('express');
+require('dotenv').config();
+const app = express();
+const fs = require('fs');
+
+
+app.get('/:filename', (req, res) => {
+    const { filename } = req.params;
+    const fileStream = fs.createReadStream(`./images/${filename}`);
+    fileStream.pipe(res);
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`listening on port ${process.env.PORT}`);
+});
