b5496a2beecf296bbd8fa2d814cf5c704458120d
rajat_malhotra <rajatmalhotra26@gmail.com>
2022-08-29 23:07:45 +0530

    using static public

--- express.js
+++ express.js
@@ -1,10 +1,11 @@
-const express = require('express');
-require('dotenv').config();
-const app = express();
 const fs = require('fs');
+require('dotenv').config();
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.send('welcome to http1');
+    res.send();
 });
 
 app.get('/:filename', (req, res) => {
