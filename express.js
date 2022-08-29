4ff41563dbf29540296ee8bae440e3b3c02e6928
rajat_malhotra <rajatmalhotra26@gmail.com>
2022-08-30 00:39:55 +0530

    https

--- express.js
+++ express.js
@@ -1,4 +1,6 @@
-const fs = require('fs');
+const fs = require('node:fs');
+const http2 = require('node:http2');
+
 require('dotenv').config();
 const express = require('express');
 const app = express();
@@ -14,6 +16,9 @@ app.get('/:filename', (req, res) => {
     fileStream.pipe(res);
 });
 
-app.listen(process.env.PORT, () => {
+http2.createServer({
+    key: fs.readFileSync('./certs/server.key'),
+    cert: fs.readFileSync('./certs/server.crt')
+}, app).listen(8080, () => {
     console.log(`listening on port ${process.env.PORT}`);
 });
