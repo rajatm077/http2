a252f70e0d1bfabf5682d376c0de6915d8e10cb5
rajat_malhotra <rajatmalhotra26@gmail.com>
2022-08-30 01:15:57 +0530

    working

--- express.js
+++ express.js
@@ -20,13 +20,12 @@ spdy
         console.log('Listening on port: ' + 3000 + '.');
     });
 
-    app.get('/', function (req, res) {
-        res.send('Serving using HTTP2!');
-      });
+app.get('/', function (req, res) {
+    res.send('Serving using HTTP2!');
+});
 
-      app.get('/:filename', (req, res) => {
-        const { filename } = req.params;
-        const fileStream = fs.createReadStream(`./images/${filename}`);
-        fileStream.pipe(res);
-    });
-    
+app.get('/:filename', (req, res) => {
+    const { filename } = req.params;
+    const fileStream = fs.createReadStream(`./images/${filename}`);
+    fileStream.pipe(res);
+});
